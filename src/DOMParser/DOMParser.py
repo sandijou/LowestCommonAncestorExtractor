73d8d49c16e63bb4f2542adc3060bc1f00e511d6
pmpc <j.sandkuehler@posteo.de>
2023-06-06 12:28:49 +0200

    Update DOMParser.py

--- src/DOMParser/DOMParser.py
+++ src/DOMParser/DOMParser.py
@@ -5,69 +5,78 @@ from lxml import etree
 
 
 
-# Parse the HTML doc downloaded by Selenium.
+# Parse the HTML doc loaded as a Datalink by Selenium.
 # Attributes: driver  = Selenium driver; extractStyle = whether style is rendered in this step
-def parseTree(driver, extractStyle):
+def parse_tree(sess_driver, extractStyle):    
     parser = etree.HTMLParser()
-    tree = etree.parse(StringIO(driver.page_source), parser)
-    root = tree
+    tree = etree.parse(StringIO(sess_driver.page_source), parser)
+    root = tree # -> lxml.etree._ElementTree, which represents the whole document, so it doesn't have a 'tag' (unclear why the copy?)
+    #root = tree.getroot() -> lxml.etree._Element
     body = tree.xpath('/html/body')[0]
     xpath = tree.getpath(body)
-    toReturn = TreeElement()
+    
+    if not xpath == '/html/body':
+        print(f"\n xpath =! /html/body: Root node: {root.getroot().tag}, body node: {body.tag}, xpath: {xpath}")
+    
+    toReturn = TreeElement() # => DOMnode self.tag = '',self.attributes = dict(), self.style = '',self.xpath = ''
     toReturn.tag = body.tag
     toReturn.xpath = xpath
     if extractStyle:
-        toReturn.style = parseStyle(driver, xpath)
-    toReturn.children = parseSeleniumTreeRec(driver, root, body.xpath('child::node()'), extractStyle)
+        toReturn.style = parseStyle(sess_driver, xpath)
+        #print('ROOT style: ', toReturn.style)    
+    toReturn.children = parseSeleniumTreeRec(sess_driver, root, body.xpath('child::node()'), extractStyle)    
     return toReturn
 
-
+# Checking if manipulating the visual text content of the html document looses actual content
+def print_string_difference(str1, str2):
+    # Find the length of the shorter string
+    length = min(len(str1), len(str2))
+    # Iterate over the characters and compare
+    #for i in range(length):
+        #if str1[i] != str2[i]:
+            #print(f"Difference at index {i}: '{str1[i]}' vs '{str2[i]}'")
+    # Print the remaining characters if one string is longer than the other
+    if len(str1) - length > 2:
+        print(f"Additional characters in str1: '{str1[length:]}'")
+    elif len(str2) - length > 2:
+        print(f"Additional characters in str2: '{str2[length:]}'")
 
 # Recursive counterpart to parseSeleniumTree
 # Attributes: driver = Selenium driver; root = root HTML element; currentElem = node investigated in this recursion
 # step; extractStyle = whether style is rendered
-def parseSeleniumTreeRec(driver, root, currentElem, extractStyle):
+def parseSeleniumTreeRec(sess_driver, root, currentElem, extractStyle):
     childList = []
     for child in currentElem:
-        if isinstance(child, str):
-            cutted = cutStrToVisibleContent(child)
-            childList.append(TextElement(cutted))
-        elif not ('Comment' in str(child.tag)):         #((str(child.tag) + '$') in getListOfUnrederedTags()) and not 
-            treeElement = TreeElement()
-            xpath = root.getpath(child)
-            treeElement.attributes = child.attrib
-            treeElement.tag = child.tag
-            treeElement.xpath = xpath
-            if extractStyle:
-                treeElement.style = parseStyle(driver, xpath)
-            treeElement.children = parseSeleniumTreeRec(driver, root, child.xpath('child::node()'), extractStyle)
-            childList.append(treeElement)
-        else:
-            None
-    return childList
-
-
-'''
-def parseSeleniumTreeRec(driver, root, currentElem, extractStyle):
-    childList = []
-    for child in currentElem:
-        if isinstance(child, str):
-            cutted = cutStrToVisibleContent(child)
-            childList.append(TextElement(cutted))
+        if isinstance(child, str): # or isinstance(child, etree._ElementUnicodeResult):
+            #cutted = cutStrToVisibleContent(child)
+            reformatted = check_headline_formatting(child)
+            
+            # Checking for content loss
+            #string1 = repr(reformatted)
+            #string2 = repr(child)
+            #print_string_difference(string1, string2)
+            #if not cutted == '' and len(cutted) < 50:
+                #print(f'\n TEXTelement: parent elem.tag {tmpTag}:  \n{repr(cutted)}\n')
+            
+            childList.append(TextElement(reformatted))
+        #elif (str(child.tag) + '$') in getListOfUnrenderedTags():
+          #print(f'found unrendered Tag: {str(child.tag)}')
         elif not ((str(child.tag) + '$') in getListOfUnrederedTags()) and not ('Comment' in str(child.tag)):
+            #print(f"Processing element child with tag: {child.tag}")
+            #tmpTag = str(child.tag)
             treeElement = TreeElement()
             xpath = root.getpath(child)
             treeElement.attributes = child.attrib
             treeElement.tag = child.tag
             treeElement.xpath = xpath
             if extractStyle:
-                treeElement.style = parseStyle(driver, xpath)
-            treeElement.children = parseSeleniumTreeRec(driver, root, child.xpath('child::node()'), extractStyle)
+                #if not hasOnlyTreeChildren(treeElement):
+                treeElement.style = parseStyle(sess_driver, xpath)
+            treeElement.children = parseSeleniumTreeRec(sess_driver, root, child.xpath('child::node()'), extractStyle)
             childList.append(treeElement)
         else:
             None
     return childList
-'''
 
 
 # Parse style of given element.
@@ -87,39 +96,18 @@ def parseStyle(driver, xpath):
     except Exception as e:
         # Print exception for debugging
         print("Exception occurred during style parsing:", str(e))
-
         # Return a fallback style object
-        return Font(1, 300, False, 'undefined') #Font(16, 400, False, 'Times New Roman')
-
+        return Font(1, 300, False, 'undefined')
 
 
 # Cut unimportant characters from text.
 # Attributes: text = input string
 def cutStrToVisibleContent(text):
-    toReturn = []
-    for c in text:
-        if ord(c) == 10 or ord(c) >= 32:
-            toReturn.append(c)
-    empty = ""
-    return cutStartAndEndWhitespaces(empty.join(toReturn).replace('\n', ' '))
+    toReturn = ''.join(c for c in text if ord(c) == 10 or ord(c) >= 32)
+    return toReturn.strip().replace('\n', ' ')
 
-
-
-# Cut whitespaces at begin and end from text.
-# Attributes: text = input string
-def cutStartAndEndWhitespaces(text):
-    firstCharStart = 0
-    lastCharEnd = (len(text) - 1)
-    for i in range(0, len(text)):
-        if ord(text[i]) != ord(' '):
-            firstCharStart = i
-            break
-    for i in range(len(text) - 1, -1, -1):
-        if ord(text[i]) != ord(' '):
-            lastCharEnd = i
-            break
-    return text[firstCharStart:(lastCharEnd + 1)]
-
+# (Cut whitespaces at begin and end from text. # Attributes: text = input string)
+## Function replaced with cutStr above (string.strip())
 
 
 # Return list of all tags + '$' not rendered in HTML documents.
@@ -154,37 +142,20 @@ def extractStyleForSubtreeRec(driver, node):
 # Removes empty nodes.
 # Attributes: node = node, of which subtree is processed
 def cleanMainContent(node):
-    newChildren = []
-    for child in node.children:
-        if isinstance(child, TextElement) and hasNoContent(child):
-            None
-        elif isinstance(child, TreeElement):
-            child = cleanMainContent(child)
-            newChildren.append(child)
-        else:
-            newChildren.append(child)
+    newChildren = [child for child in node.children if not (isinstance(child, TextElement) and hasNoContent(child))]
+    for i, child in enumerate(newChildren):
+        if isinstance(child, TreeElement):
+            newChildren[i] = cleanMainContent(child)
     node.children = newChildren
     return node
-
-
 
 # Checks, whether a TextElement node has no content
 # Attributes: textNode = TextElement which is investigated
 def hasNoContent(textNode):
-    toReturn = True
-    for c in textNode.text:
-        if ord(c) > 32:
-            toReturn = False
-    return toReturn
-
-
+    return not any(ord(c) > 32 for c in textNode.text)
 
 # Checks, whether a nodes children are all TreeElements and do not hold a single TextElement.
 # Attributes: node = node, of which subtree is investigated
 def hasOnlyTreeChildren(node):
-    toReturn = True
-    for child in node.children:
-        if isinstance(child, TextElement):
-            toReturn = False
-    return toReturn
+    return not any(isinstance(child, TextElement) for child in node.children)
 
